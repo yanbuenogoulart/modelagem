758b09ed4e9434e65acb48db22855ad3d72f49ec
YAN BUENO GOULART <yan_goulart@estudante.sesisenai.org.br>
2025-07-11 12:39:50 -0300

    comentando

--- model/rel.js
+++ model/rel.js
@@ -2,21 +2,27 @@ const Compras = require('./Compras');
 const Usuarios = require('./Usuarios');
 const Produtos = require('./Produtos');
 
+
+// compras pertence a usuarios
 Compras.belongsTo(Usuarios, {
     foreignKey: 'idUsuario',
     as: 'idUsuarios'
 })
 
+// compras podem ter varios produtos
 Compras.hasMany(Produtos, {
     foreignKey: 'idCompras',
     as: 'idProdutos'
 })
 
+
+// usuarios podem ter varias compras
 Usuarios.hasMany(Compras, {
     foreignKey: 'idCompras',
     as: 'idCompras'
 })
 
+// produtos pertencem a compras
 Produtos.belongsTo(Compras, {
     foreignKey: 'idCompras',
     as: 'idCompras'
